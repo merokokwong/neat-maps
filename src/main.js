4be42ff5f161478ad829f92eb18323aa12d7af64
Ka Yan Kwong <>
2019-09-13 13:54:49 +0800

    Hide API key in .env

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
+import dotenv from "dotenv";
+dotenv.config();
 import store from "./store";
 import "@/assets/css/main.scss";
-import 'font-awesome/css/font-awesome.css';
+import "font-awesome/css/font-awesome.css";
 import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyCzol4NudU170oJ_SCLRIqjPxYsW7vOthE",
+    key: process.env.VUE_APP_API_KEY,
     libraries: "places" // necessary for places input
   }
 });
 
-
 let app = new Vue({
   store,
   render: h => h(App)
-}).$mount('#app')
-
+}).$mount("#app");
 
 global.vm = app;
